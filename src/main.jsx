47414a81332751aebf79981754e14b29f6a5b098
_bangcumtay <58516805+ducbang26@users.noreply.github.com>
2026-06-29 03:24:49 +0000

    fix deploy 2

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,25 @@
 import "./styles/app.scss";
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-if (import.meta.env.VITE_ENABLE_MSW === "true") {
-  await worker.start({
-    serviceWorker: {
-      url: "/mockServiceWorker.js",
-    },
-    onUnhandledRequest: "bypass",
-  });
-}
 import App from "./App";
-import { worker } from "./mocks/browser";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+async function startApp() {
+  if (import.meta.env.VITE_ENABLE_MSW === "true") {
+    const { worker } = await import("./mocks/browser");
+
+    await worker.start({
+      serviceWorker: {
+        url: "/mockServiceWorker.js",
+      },
+      onUnhandledRequest: "bypass",
+    });
+  }
+
+  ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
+
+startApp();
